3ffee3d29cec9e3e33e4a7888147ae40e8bc1907
jvalbuquerque <jv.albuquerque@live.com>
2021-06-21 17:45:23 -0300

    Basic movement

---
+++ Asteroids/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    static public GameController Instance;
+
+    private void Awake()
+    {
+        Singleton();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Singleton()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Asteroids/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private float maxVelocity = 3;
+    [SerializeField] private float moveForce = 10;
+    [SerializeField] private float rotationForce = 100;
+
+    //Verify if the ship is to accelerate
+    private bool accelerating = false;
+    //If the rotation is clockwise (1) or not (-1)
+    private float clockwise = 0;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(accelerating)
+        {
+            MoveShip();
+        }
+
+        if(clockwise != 0)
+        {
+            RotateShip();
+        }
+    }
+
+    private void MoveShip()
+    {
+        Vector2 force = transform.up * moveForce;
+
+        rb.AddForce(force);
+
+        ClampVelocity();
+    }
+
+    private void ClampVelocity()
+    {
+        float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+        float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+
+        rb.velocity = new Vector2(x, y);
+    }
+
+    private void RotateShip()
+    {
+        transform.RotateAround(transform.position, Vector3.forward, clockwise* rotationForce * Time.deltaTime);
+    }
+
+    public void OnRotate(InputAction.CallbackContext value)
+    {
+        clockwise = value.ReadValue<float>();
+
+        //only to make the right button go clockwise
+        clockwise *= -1;
+    }
+
+    public void OnMove(InputAction.CallbackContext value)
+    {
+        if(value.started)
+        {
+            accelerating = true;
+        }
+        if (value.canceled)
+        {
+            accelerating = false;
+        }
+    }
+}
